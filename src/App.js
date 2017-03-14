b18af6e0993706b0e08952d274fdfdfd906c1812
Matthew Holman <mateoholman@gmail.com>
2017-03-14 16:48:11 -0500

    Added react-router-dom and added routing to the App

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import Home from './components/Home';
+import BinarySearch from './components/BinarySearch';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <Router>
+        <div>
+          <Route exact path='/' component={Home} />
+          <Route path='/binary' component={BinarySearch} />
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      </Router>
     );
   }
 }
--- src/components/binarySearch.js
+++ src/components/binarySearch.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
 
-class binarySearch extends Component {
+class BinarySearch extends Component {
   render() {
     return (
+      <div>
       <h1>Binary Search</h1>
+      <p>Binary search narrows the search field by half each time.</p>
+      </div>
     );
   }
 }
+
+export default BinarySearch;
