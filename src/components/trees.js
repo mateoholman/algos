52021fb202a1106e6e7a9d379a857d44cd816e25
Matthew Holman <mateoholman@gmail.com>
2017-04-30 21:12:47 -0500

    Added trees component

---
+++ src/components/trees.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Trees extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Trees</h1>
+        <p>Trees...</p>
+      </div>
+    );
+  }
+}
+
+export default BinarySearch;
