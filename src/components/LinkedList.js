a26569af6a2e4a6d2af7767edf5a4efd8cebe9d7
Matthew Holman <mateoholman@gmail.com>
2017-05-02 12:57:24 -0500

    Added constructor functions to LinkedList

--- src/components/LinkedList.js
+++ src/components/LinkedList.js
@@ -1,6 +1,20 @@
 import React, { Component } from 'react';
 
 class LinkedList extends Component {
+
+  //Our LinkedList constructor
+  function LinkedList() {
+    this.head = null;
+    this.tail = null;
+  }
+
+  //Our Node constructor
+  function Node(value, next, prev) {
+    this.value = value;
+    this.next = next;
+    this.prev = prev;
+  }
+
   render() {
     return (
       <div>
