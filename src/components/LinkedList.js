cccff842438d9e901f3699946fd2e2d696bd5b54
Matthew Holman <mateoholman@gmail.com>
2017-05-02 12:34:55 -0500

    Added LinkedList component

---
+++ src/components/LinkedList.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class LinkedList extends Component {
+  render() {
+    return (
+      <div>
+        <h1>LinkedList</h1>
+        <p>LinkedList...</p>
+      </div>
+    );
+  }
+}
+
+export default LinkedList;
