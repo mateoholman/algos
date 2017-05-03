5c64fb7b50e479da9a258496b410de591a8fa87d
Matthew Holman <mateoholman@gmail.com>
2017-05-03 14:07:13 -0500

    Connected form submit action to function

--- src/components/LinkedList.js
+++ src/components/LinkedList.js
@@ -7,6 +7,8 @@ class LinkedList extends Component {
 
     this.state = {
         listName: '',
+        numNodes: 0,
+        currentLL: [],
     };
   }
 
@@ -23,19 +25,37 @@ class LinkedList extends Component {
     this.prev = prev;
   }
 
-  handleChange(e) {
+  handleNameChange(e) {
     this.setState({ listName: e.target.value });
   }
 
-  createLinkedList(listInfo){
-    console.log(listInfo);
+  handleNodeChange(e) {
+    this.setState({ numNodes: e.target.value});
+  }
+
+  handleSubmit(e){
+    e.preventDefault();
+    console.log(this.state.listName);
+    console.log(this.state.numNodes);
+    const ll = new LinkedList();
+    console.log(ll);
+  }
+
+  renderLinkedList() {
+    //console.log(this.state.currentLL.length);
+    if (this.state.currentLL.length > 0) {
+      return(<h2>There is a Linked List!</h2>);
+    }
+    else {
+      return(<h2>No Current Linked Lists</h2>);
+    }
   }
 
   render() {
     return (
       <div>
         <h1>LinkedList</h1>
-        <Form horizontal>
+        <Form horizontal onSubmit={this.handleSubmit.bind(this)}>
           <FormGroup controlId="listName">
             <Col componentClass={ControlLabel} sm={2}>
               List Name
@@ -45,7 +65,7 @@ class LinkedList extends Component {
                 type="text"
                 value={this.state.ListNameValue}
                 placeholder="Enter List Name"
-                onChange={this.handleChange.bind(this)}
+                onChange={this.handleNameChange.bind(this)}
               />
             </Col>
           </FormGroup>
@@ -54,7 +74,7 @@ class LinkedList extends Component {
               # of Nodes
             </Col>
             <Col sm={4}>
-              <FormControl componentClass="select" placeholder="1">
+              <FormControl componentClass="select" placeholder="1" onChange={this.handleNodeChange.bind(this)}>
                 <option value="1">1</option>
                 <option value="2">2</option>
                 <option value="3">3</option>
@@ -65,13 +85,13 @@ class LinkedList extends Component {
           </FormGroup>
           <FormGroup>
             <Col smOffset={2} sm={4}>
-              <Button type="submit" onSubmit={this.createLinkedList.bind(this)}>
+              <Button type="submit">
                 Create
               </Button>
             </Col>
           </FormGroup>
-
         </Form>
+        {this.renderLinkedList()}
       </div>
     );
   }
