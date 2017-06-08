0eb12a6fee66820557778a6c1d8e4bcfbb29bf27
Matthew Holman <mateoholman@gmail.com>
2017-06-08 16:32:59 -0500

    Added seek and destroy algo

---
+++ test/seekAndDestroy.js
@@ -0,0 +1,14 @@
+function destroyer(arr) {
+  //O: Array with all elements matching the passed arguments
+  //I: An array where the first element is an array and the next elements are arguments to be removed
+  //Take the array we want to manipulate from the given array
+  let newArr = arr[0];
+
+  function filterer(){
+
+  }
+
+  newArr.filter(filterer());
+}
+
+destroyer([1, 2, 3, 1, 2, 3], 2, 3);
