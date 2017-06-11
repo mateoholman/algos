5a66d6e106a4a213747cab83242c0f8eb2ba0906
Matthew Holman <mateoholman@gmail.com>
2017-06-10 20:35:27 -0500

    Added the layout for the Cesar's Cipher algo

---
+++ test/cesarsCipher.js
@@ -0,0 +1,20 @@
+function rot13(str) {
+  //O: A decoded string
+  //I: An encoded string where the values of the letters are shifted by 13
+  //places.
+
+  //Slice the string into an array of characters
+
+  //Xform the array values to their unicode equivalent
+
+  //Shift the characters by 13 places
+
+  //Xform the array of unicode values back to characters
+
+  //Combine the array back to a string
+
+  return str;
+}
+
+// Change the inputs below to test
+rot13("SERR PBQR PNZC");
