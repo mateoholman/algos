8349f7f71c78a4f5e6142f5cc5b120e638688a2f
Matthew Holman <mateoholman@gmail.com>
2017-06-11 22:03:27 -0500

    Progress on Cesar's Cipher

--- test/cesarsCipher.js
+++ test/cesarsCipher.js
@@ -4,15 +4,31 @@ function rot13(str) {
   //places.
 
   //Slice the string into an array of characters
+  let codedAry = [];
+  let decodedAry = [];
+  let newStrAry = [];
 
+  for(i=0;i<str.length;i++){
+    codedAry[i] = str.charCodeAt(i);
+  }
   //Xform the array values to their unicode equivalent
 
-  //Shift the characters by 13 places
-
+  //Shift the characters by 13 places, but only if they are letters
+  decodedAry = codedAry.map((e) => {
+    if(e > 58 && e < 85){
+      return e-13;
+    }
+      else {
+        return e;
+      }
+  });
   //Xform the array of unicode values back to characters
 
   //Combine the array back to a string
-
+  newStrAry = decodedAry.map((e) => {
+    return String.fromCharCode(e)});
+  str = newStrAry.join("").toString();
+  console.log("The new string is: " + str);
   return str;
 }
 
