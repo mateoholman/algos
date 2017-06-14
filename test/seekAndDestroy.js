3445f64d93823f31ba3c91e2588a0fba6bd0ccb8
Matthew Holman <mateoholman@gmail.com>
2017-06-13 21:08:51 -0500

    Solved seek & destroy

--- test/seekAndDestroy.js
+++ test/seekAndDestroy.js
@@ -4,29 +4,37 @@ function destroyer(arr) {
   //I: An array where the first element is an array and the next elements are arguments to be removed
 
   //Pull the arguments passed in to the function and put them in an array
-  var args = Array.from(arguments);
-  //Take the array we want to manipulate from the given array
-  let newArr = args[0];
-  let filterNumArr = [];
-  let filteredArr = [];
+  // var args = Array.from(arguments);
+  // //Take the array we want to manipulate from the given array
+  // let newArr = args[0];
+  // let filterNumArr = [];
+  // let filteredArr = [];
+  //
+  // for(k=1;k<args.length;k++){
+  //   filterNumArr = args[k];
+  // }
+  //
+  // function filterer(){
+  //   return !filterNumArr;
+  // }
+  //
+  // // Cycle through the arguments passed in to destroyer
+  //   filteredArr = newArr.filter(filterer);
+  //
+  // for(j=0;j<filteredArr.length;j++){
+  //   console.log(filteredArr[j]);
+  // }
+  //
+  // //return the filtered array
+  // return filteredArr;
 
-  for(k=1;k<args.length;k++){
-    filterNumArr = args[k];
-  }
-
-  function filterer(){
-    return !filterNumArr;
-  }
-
-  // Cycle through the arguments passed in to destroyer
-    filteredArr = newArr.filter(filterer);
-
-  for(j=0;j<filteredArr.length;j++){
-    console.log(filteredArr[j]);
-  }
-
-  //return the filtered array
-  return filteredArr;
+  function destroyer(arr) {
+  var args = Array.prototype.slice.call(arguments);
+  args.splice(0,1);
+  return arr.filter(function(element) {
+    return args.indexOf(element) === -1;
+  });
+}
 
 }
 
