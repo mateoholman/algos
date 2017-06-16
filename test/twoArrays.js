eae569f2b799fc06c0a41c4d78f032edb8ad3bb6
Matthew Holman <mateoholman@gmail.com>
2017-06-16 13:49:18 -0500

    Added new algo for comparing two arrays

---
+++ test/twoArrays.js
@@ -0,0 +1,10 @@
+function diffArray(arr1, arr2) {
+  //O: An array that only contains items not found in both arrays
+  //I: Two arrays
+
+  var newArr = [];
+  // Same, same; but different.
+  return newArr;
+}
+
+diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
